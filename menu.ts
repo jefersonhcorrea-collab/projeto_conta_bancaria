5b27f59e67e5a0400a0fe3bfd5a9d2d1d2dac50f
Jeferson correa <jefersonhcorrea@hotmail.com>
2025-11-06 18:52:20 -0300

    Menu-colors

--- menu.ts
+++ menu.ts
@@ -1,10 +1,13 @@
 import readlinesync = require("readline-sync");
+import { colors } from "./src/util/colors";
 
 export function main(){
     let opcao: number;
 
     while (true) {
-        console.log("****************************************************");
+
+        console.log(colors.bg.black, colors.fg.yellow,
+                    "****************************************************");
         console.log("                                                    ");
         console.log("               BANCO DO BRAZIL COM Z                ");
         console.log("                                                    ");       
@@ -21,52 +24,66 @@ export function main(){
         console.log("               9 - Sair                             ");
         console.log("                                                    ");
         console.log("****************************************************");
+        console.log("                                                    ",
+        colors.reset);
 
         console.log("Entre com a opcao desejada: ");
         opcao = readlinesync.questionInt("");
 
         if (opcao == 9){
-            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            console.log(colors.fg.greenstrong,
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
+            console.log(colors.reset, "")
             process.exit(0);
         }
 
         switch (opcao) {
             case 1:
-                console.log("\n\nCriar Conta\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nCriar Conta\n\n", colors.reset);
 
                 break;
             case 2:
-                console.log("\n\nListar todas as Contas\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nListar todas as Contas\n\n", colors.reset);
 
                 break            
             case 3:
-                console.log("\n\nConsultar dados da Conta - por número\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
                 break;            
             case 4: 
-                console.log("\n\nAtualizar dados da Conta\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
                 break;
             case 5:
-                console.log("\n\nApagar uma conta\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nApagar uma conta\n\n", colors.reset);
 
                 break;
             case 6:
-                console.log("\n\nSaque\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nSaque\n\n", colors.reset);
 
                 break;
             case 7:
-                console.log("\n\nDepósito\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nDepósito\n\n", colors.reset);
 
                 break;
             case 8:
-                console.log("\n\nTransferência entre Contas\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
 
                 break;
             default:
-                console.log("\n\nOpção inválida!\n\n");
+                console.log(colors.fg.whitestrong,
+                    "\n\nOpção inválida!\n\n", colors.reset);
 
+                keyPress()
                 break;
         }
     }
@@ -74,10 +91,17 @@ export function main(){
 
 export function sobre(): void{
     console.log("\n****************************************************");
-    console.log("projeto Desenvolvido por: Jeferson Corrêa");
-    console.log("Email- jefersonhcorrea@hotmail.com");
-    console.log("github.com/conteudoGeneration");
+    console.log("Projeto Desenvolvido por: Jeferson Corrêa");
+    console.log("Email- jefersonc@genstudents.org");
+    console.log("github.com/jefersonhcorrea-collab");
     console.log("****************************************************");
 }
+
+function keyPress(): void{
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
 
 main();
